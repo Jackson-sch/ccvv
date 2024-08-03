03e43ab12c79869b91e1f59a0754db71b2b6bd43
Jackson-sch <darwinjackson.12@gmail.com>
2024-08-03 18:02:27 -0500

    webhooks

--- src/app/api/webhooks/route.jsx
+++ src/app/api/webhooks/route.jsx
@@ -1,8 +1,10 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import mongoose from 'mongoose';
-import User from "@/models/User/User";export async function POST(req) {
-  
+import User from "@/models/User/User";
+
+export async function POST(req) {
+
   // Obtén el secreto del webhook desde las variables de entorno
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
