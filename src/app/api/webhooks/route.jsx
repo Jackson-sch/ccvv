0fd59d6e3fe34694fdc21b5213ad1618b1738fcd
Jackson-sch <darwinjackson.12@gmail.com>
2024-08-03 19:04:18 -0500

    p

--- src/app/api/webhooks/route.jsx
+++ src/app/api/webhooks/route.jsx
@@ -1,15 +1,16 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
-import mongoose from 'mongoose';
 import User from "@/models/User/User";
+import { dbConnect } from '@/lib/mongoose';
 
 export async function POST(req) {
-
   // Obtén el secreto del webhook desde las variables de entorno
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Por favor, añade WEBHOOK_SECRET desde el Dashboard de Clerk a .env o .env.local');
+    return new Response('Por favor, añade WEBHOOK_SECRET desde el Dashboard de Clerk a .env o .env.local', {
+      status: 500,
+    });
   }
 
   // Obtén los headers
@@ -42,43 +43,30 @@ export async function POST(req) {
       "svix-signature": svix_signature,
     });
 
-    // Esto es opcional si quieres manejar el caso en el que no sea del tipo WebhookEvent
-    if (evt instanceof WebhookEvent) {
-      // Procesa el evento del webhook
-      const { id, ...userData } = evt.data; // Extrae los datos necesarios del evento
-      const eventType = evt.type;
-      console.log(`Webhook con ID de ${id} y tipo de ${eventType}`);
-      console.log('Cuerpo del webhook:', body);
+    // Procesa el evento del webhook
+    const { id, ...userData } = evt.data; // Extrae los datos necesarios del evento
+    const eventType = evt.type;
+    console.log(`Webhook con ID de ${id} y tipo de ${eventType}`);
+    console.log('Cuerpo del webhook:', body);
 
-      // Conecta a MongoDB si aún no lo has hecho
-      if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-      }
+    // Conecta a MongoDB si aún no lo has hecho
+    await dbConnect();
 
-      try {
-        if (eventType === 'user.created') {
-          // Si el evento es 'user.created', registra el nuevo usuario en la base de datos
-          const newUser = new User(userData);
-          await newUser.save();
-          console.log(`Nuevo usuario creado: ${newUser._id}`);
-        } else if (eventType === 'user.updated') {
-          // Si el evento es 'user.updated', actualiza el usuario en la base de datos
-          await User.updateOne({ id }, userData);
-          console.log(`Usuario actualizado: ${id}`);
-        }
-      } catch (error) {
-        console.error('Error guardando en la base de datos:', error);
-        return new Response('Ocurrió un error', {
-          status: 500,
-        });
+    try {
+      if (eventType === 'user.created') {
+        // Si el evento es 'user.created', registra el nuevo usuario en la base de datos
+        const newUser = new User(userData);
+        await newUser.save();
+        console.log(`Nuevo usuario creado: ${newUser._id}`);
+      } else if (eventType === 'user.updated') {
+        // Si el evento es 'user.updated', actualiza el usuario en la base de datos
+        await User.updateOne({ id }, userData);
+        console.log(`Usuario actualizado: ${id}`);
       }
-    } else {
-      console.error('El evento no es del tipo WebhookEvent');
+    } catch (error) {
+      console.error('Error guardando en la base de datos:', error);
       return new Response('Ocurrió un error', {
-        status: 400,
+        status: 500,
       });
     }
   } catch (err) {
