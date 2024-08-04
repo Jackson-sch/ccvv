0bab2d33b69952ea8a72d7eb78bf770b26c0fb44
Jackson-sch <darwinjackson.12@gmail.com>
2024-08-03 20:24:26 -0500

    status

--- src/app/api/webhooks/route.jsx
+++ src/app/api/webhooks/route.jsx
@@ -1,13 +1,15 @@
-import { Webhook } from 'svix';
-import { headers } from 'next/headers';
+import { Webhook } from "svix";
+import { headers } from "next/headers";
 import User from "@/models/User/User";
-import { dbConnect } from '@/lib/mongoose';
+import { dbConnect } from "@/lib/mongoose";
 
 export async function POST(req) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Por favor, añade WEBHOOK_SECRET desde el Dashboard de Clerk a .env o .env.local');
+    throw new Error(
+      "Por favor, añade WEBHOOK_SECRET desde el Dashboard de Clerk a .env o .env.local"
+    );
   }
 
   const headerPayload = headers();
@@ -16,7 +18,7 @@ export async function POST(req) {
   const svix_signature = headerPayload.get("svix-signature");
 
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Ocurrió un error -- no hay headers de svix', {
+    return new Response("Ocurrió un error -- no hay headers de svix", {
       status: 400,
     });
   }
@@ -29,44 +31,56 @@ export async function POST(req) {
   try {
     // Verificar el webhook
     const evt = wh.verify(body, {
-      'svix-id': svix_id,
-      'svix-timestamp': svix_timestamp,
-      'svix-signature': svix_signature,
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
     });
 
-    const { id, email_addresses, first_name, last_name, profile_image_url, ...userData } = evt.data;
+    const {
+      id,
+      email_addresses,
+      first_name,
+      last_name,
+      profile_image_url,
+      ...userData
+    } = evt.data;
     const eventType = evt.type;
 
-    const email = email_addresses.length > 0 ? email_addresses[0].email_address : undefined;
+    const email =
+      email_addresses.length > 0 ? email_addresses[0].email_address : undefined;
+    const status =
+      email_addresses.length > 0
+        ? email_addresses[0].verification.status
+        : undefined;
     const clerkId = id;
     const name = `${first_name} ${last_name}`;
     const imageUrl = profile_image_url;
 
     if (!email || !clerkId) {
-      throw new Error('Faltan campos necesarios para la base de datos');
+      throw new Error("Faltan campos necesarios para la base de datos");
     }
 
     dbConnect();
 
-    if (eventType === 'user.created') {
+    if (eventType === "user.created") {
       const newUser = new User({
         clerkId,
         email,
         name,
         imageUrl,
+        status,
         ...userData,
       });
       await newUser.save();
       console.log(`Nuevo usuario creado: ${newUser._id}`);
-    } else if (eventType === 'user.updated') {
-      await User.updateOne({ clerkId }, { name, imageUrl, email, ...userData });
+    } else if (eventType === "user.updated") {
+      await User.updateOne({ clerkId }, { name, imageUrl, email, status, ...userData });
       console.log(`Usuario actualizado: ${clerkId}`);
     }
-
   } catch (err) {
-    console.error('Error verificando webhook:', err);
-    return new Response('Ocurrió un error', { status: 400 });
+    console.error("Error verificando webhook:", err);
+    return new Response("Ocurrió un error", { status: 400 });
   }
 
-  return new Response('', { status: 200 });
+  return new Response("", { status: 200 });
 }
